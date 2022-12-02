152326bc7c31dab1248f4ba53b36a5fa09cd3b8b
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2022-12-02 00:09:02 -0500

    Part 2 completed! Day 1 of rust done! 😁

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,9 +1,15 @@
 fn main() {
     let input = include_str!("../input/day1.txt");
-    let max_calories_elve: Option<u32> = input
+    let mut max_calories_elve: Vec<u32> = input
         .split("\n\n")
         .map(|elve_foods| elve_foods.split("\n").flat_map(|x| x.parse::<u32>()).sum())
-        .max();
+        .collect();
 
-    println!("{:?}", max_calories_elve)
+    max_calories_elve.sort_by(|a, b| b.cmp(a));
+
+    println!("Part one: {:?}", max_calories_elve[0]);
+    println!(
+        "Part two: {:?}",
+        max_calories_elve.iter().take(3).sum::<u32>()
+    );
 }

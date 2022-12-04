d02e71b3e6aacd5ee32623788dcb99f923157487
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2022-12-04 13:57:57 -0500

    Day 4 done! (in Python)

--- src/bin/day4.py
+++ src/bin/day4.py
@@ -18,11 +18,23 @@ def part_1(path: Path) -> None:
 
 def part_2(path: Path) -> None:
     lines = path.read_text().splitlines()
+    count = 0
+    for line in lines:
+        ((i1, j1), (i2, j2)) = [
+            map(int, _range.split('-')) for _range in line.split(',')
+        ]
+        # print(i1, j1, i2, j2)
+        range_1 = set(range(i1, j1 + 1))
+        range_2 = set(range(i2, j2 + 1))
+        # print(range_1.intersection(range_2))
+        if len(range_1.intersection(range_2)) > 0:
+            count += 1
+    print(count)
 
 
 def main():
     part_1(Path('src/input/day4_1'))
-    # part_2(Path('src/input/day3_2'))
+    part_2(Path('src/input/day4_1'))
 
 
 if __name__ == '__main__':
